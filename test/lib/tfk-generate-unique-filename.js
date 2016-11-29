2e4ac59253adf005a86321c62796f22a5e8bbce2
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-29 08:41:38 +0100

    Updates test

--- test/lib/tfk-generate-unique-filename.js
+++ test/lib/tfk-generate-unique-filename.js
@@ -7,3 +7,7 @@ const uniqueFile = unique('.pdf')
 tap.ok(unique(), 'generates unique on empty')
 
 tap.equal(true, uniqueFile.indexOf('.pdf') > -1, 'generates file with correct ending')
+
+tap.equal(unique().length, 36, 'id without ending has correct length')
+
+tap.equal(unique('.pdf').length, 40, 'id with ending has correct length')
